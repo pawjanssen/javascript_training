6469eb0753bc2cceeb3b9c5d77237ecb2436970b
Jeroen Dadema <jeroen.dadema@atos.net>
2013-11-08 14:14:43 +0100

    gebruiker toevoegen

--- client/js/app/controller/GebruikersController.js
+++ client/js/app/controller/GebruikersController.js
@@ -9,6 +9,14 @@ define(['app/view/GebruikersView', 'app/service/GebruikersStorage'], function(Ge
         });
     };
 
+    GebruikersController.prototype.saveGebruiker = function(gebruikers) {
+        GebruikersStorage.saveGebruiker(gebruikers, function() {
+            GebruikersView.renderSuccessMessage("Het opslaan van de gebruiker met naam '" + gebruikers.naam + "' is gelukt.");
+        }, function() {
+            GebruikersView.renderErrorMessage("Het opslaan van de gebruiker met naam '" + gebruikers.naam + "' is mislukt, probeer opnieuw.");
+        });
+    };
+
     var gebruikersControllerInstance = new GebruikersController();
 
     return {
