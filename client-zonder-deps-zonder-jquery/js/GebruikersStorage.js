03b65ff784105a70a7669c7fde3bc3e9f587abc1
Jeroen Dadema <jeroen.dadema@gmail.com>
2013-11-22 15:12:29 +0100

    non jquery fix

--- client-zonder-deps-zonder-jquery/js/GebruikersStorage.js
+++ client-zonder-deps-zonder-jquery/js/GebruikersStorage.js
@@ -10,3 +10,7 @@ GebruikerStorage.prototype.getAll = function(successCallBack, failCallBack) {
 GebruikerStorage.prototype.get = function(gebruikersID, successCallBack, failCallBack) {
     this.settingsInstance.getJSON(this.baseURL + "/" + gebruikersID, successCallBack, failCallBack);
 };
+
+GebruikerStorage.prototype.saveGebruiker = function (gebruiker, successCallBack, failCallBack) {
+    this.settingsInstance.sendJSONToServer("POST", this.baseURL, gebruiker, successCallBack, failCallBack);
+};
--- client-zonder-deps-zonder-jquery/js/GebruikersView.js
+++ client-zonder-deps-zonder-jquery/js/GebruikersView.js
@@ -3,15 +3,84 @@ function GebruikersView(gebruikersStorageInstance) {
 }
 
 GebruikersView.prototype.renderTemplate = function() {
-    $('#pageTitle').text("Gebruikers");
-    $('#page').load("gebruikersPage.html");
-};
+    var _this = this;
+    document.getElementById("pageTitle").text = "Gebruikers";
+    settingsInstance.loadTemplate(document.getElementById('page'), "gebruikersPage.html", function() {
+        _this.gebruikersStorageInstance.getAll(function (gebruikers) {
+            _this.renderGebruikers(gebruikers);
+        }, function() {
+            _this.renderError();
+        });
+    });
+    settingsInstance.loadTemplate(document.getElementById('myModal'), "gebruikerToevoegen.html", function() {
+        _this.eventHandlersgebruikerToevoegenToepassen();
+    });
+}
 
 GebruikersView.prototype.renderGebruikers = function(gebruikers) {
-    console.log(gebruikers);
+    var templateLI = document.querySelector("#gebruikerslijst li.clonable");
+    document.querySelectorAll("#gebruikerslijst li").remove();
+    var gebruikersLijst = document.getElementById("gebruikerslijst");
 
+    gebruikers.map(function (value) {
+        var liClone = templateLI.cloneNode(true);
+        liClone.querySelector("span.naam").innerHTML = value.naam;
+        liClone.querySelector("span.gebruikersnaam").innerHTML = value.gebruikersnaam;
+        liClone.setAttribute("userid", value.id);
+        liClone.classList.remove("clonable");
+        liClone.classList.add("gebruikerLi");
+
+        gebruikersLijst.appendChild(liClone);
+    })
+
+}
+
+GebruikersView.prototype.showGebruikerToevoegenModal = function() {
+    document.getElementById("myModal").style.display = "block";
+    document.getElementById("myModal").classList.add("in");
 };
 
-GebruikersView.prototype.renderError = function() {
-
+GebruikersView.prototype.hideGebruikerToevoegenModal = function() {
+    document.getElementById("myModal").removeAttribute("style");
+    document.getElementById("myModal").classList.remove("in");
 };
+
+GebruikersView.prototype.eventHandlersgebruikerToevoegenToepassen = function () {
+    var _this = this;
+
+    var closeModalElements = document.querySelectorAll(".closeModal");
+
+    for (var i = 0; i < closeModalElements.length; ++i) {
+        var item = closeModalElements[i];
+
+        item.addEventListener("click", function(event) {
+            _this.hideGebruikerToevoegenModal();
+            event.preventDefault();
+            return false;
+        }, false);
+    };
+
+    document.getElementById("nieuwegebruikerLink").addEventListener("click", function(event) {
+        _this.showGebruikerToevoegenModal();
+        event.preventDefault();
+    }, false);
+
+    document.getElementById("saveButton").addEventListener("click", function(event) {
+        var nieuweGebruiker = { "todos": [] };
+
+        nieuweGebruiker.titel = document.getElementById('gebruikerNaam').value;
+        nieuweGebruiker.priority = document.getElementById('gebruikerGebruikersnaam').value;
+
+        _this.gebruikersStorageInstance.saveGebruiker(nieuweGebruiker, function() {
+            _this.renderSuccessMessage("Het opslaan van de gebruiker met titel '" + nieuweGebruiker.naam + "' is gelukt.");
+        }, function() {
+            _this.renderErrorMessage("Het opslaan van de gebruiker met titel '" + nieuweGebruiker.naam + "' is mislukt, probeer opnieuw.");
+        });
+
+        _this.hideGebruikerToevoegenModal();
+        event.preventDefault();
+    }, false);
+}
+
+GebruikersView.prototype.renderSuccessMessage = function() {};
+GebruikersView.prototype.renderErrorMessage = function() {};
