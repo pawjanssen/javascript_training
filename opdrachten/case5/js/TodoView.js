62d72176b8ef051f360b71d1f41026db2f2a1c93
Jeroen Dadema <jeroen.dadema@gmail.com>
2013-12-06 13:35:26 +0100

    case 5

--- opdrachten/case5/js/TodoView.js
+++ opdrachten/case5/js/TodoView.js
@@ -8,20 +8,19 @@ TodoView.prototype.renderTemplate = function() {
 }
 
 TodoView.prototype.todosDraggable = function () {
-    $("div.todoDrag").draggable({ revert: true ,
+    $("div.todoDrag.high").draggable({ revert: true ,
+        helper: "clone"});
+
+    $("div.todoDrag.low").draggable({ revert: true ,
+        containment: ".dragContainer",
         helper: "clone"});
 };
 
 TodoView.prototype.gebruikersDroppable = function () {
-    var _this = this;
     $("#gebruikerslijst li").droppable({
         hoverClass: "todoOverGebruiker",
         drop: function( event, ui ) {
-            _this.todoStorageInstance.moveTodo($(this).attr("userid"), ui.draggable.attr("todoid"), function() {
-                _this.renderSuccessMessage("Het assignen van de todo is gelukt");
-            }, function(){
-                _this.renderErrorMessage("Het assignen van de todo is mislukt");
-            });
+            console.log(this);
         }
     });
 };
--- opdrachten/case5/js/init.js
+++ opdrachten/case5/js/init.js
@@ -1,1 +1,6 @@
+var todoViewInstance = new TodoView();
 
+window.addEventListener("load", function() {
+    todoViewInstance.todosDraggable();
+    todoViewInstance.gebruikersDroppable();
+});
