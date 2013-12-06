4ef9835989e8df2224dc3fde38e19773a4900cc4
Pascal Janssen <gZ24hGb127>
2013-12-06 15:44:04 +0100

    Case 3

---
+++ opdrachten/case4/js-uitwerkingen/gebruikers.js
@@ -0,0 +1,17 @@
+var gebruikers = [
+    {
+        "id": 1,
+        "naam": "J. Script",
+        "gebruikersnaam": "jscript"
+    },
+    {
+        "id": 2,
+        "naam": "C. Scherp",
+        "gebruikersnaam": "cscherp"
+    },
+    {
+        "id": 3,
+        "naam": "S. Cala",
+        "gebruikersnaam": "scala"
+    }
+];
---
+++ opdrachten/case4/js-uitwerkingen/todos.js
@@ -0,0 +1,14 @@
+var todos = [
+    {
+        "id": 1,
+        "titel": "Afmaken opdracht 1",
+        "created": "22-11-2013",
+        "priority": "high"
+    },
+    {
+        "id": 2,
+        "titel": "Afmaken opdracht 2",
+        "created": "22-11-2013",
+        "priority": "low"
+    }
+];
